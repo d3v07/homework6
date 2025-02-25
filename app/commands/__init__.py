3ff576cd17e10fa01071747600f9c1da4288d529
d3v07 <trivedidev16.com>
2025-02-25 18:33:20 -0500

    refactor: update Command and CommandHandler implementation and docs

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,26 +1,39 @@
+"""Defines the Command base class and CommandHandler for registering/executing commands."""
+
 from abc import ABC, abstractmethod
 
 class Command(ABC):
+    """Abstract base class for all commands."""
     @abstractmethod
-    def execute(self, *args):
-        """Abstract method to be implemented by all commands."""
-        print("Executing abstract Command")
+    def execute(self):
+        """Execute the command."""
         pass
 
 class CommandHandler:
+    """Registers and executes Command instances by name."""
     def __init__(self):
         self.commands = {}
 
-    def register_command(self, command_name: str, command: Command):
-        """Registers a command with its corresponding handler."""
-        self.commands[command_name] = command
+    def register_command(self, command_name: str, command_instance: Command):
+        """Register a command instance under a given name."""
+        self.commands[command_name] = command_instance
 
-    def execute_command(self, command_name: str, *args):
-        """
-        Executes the registered command with given arguments.
-        Uses EAFP (Easier to Ask for Forgiveness than Permission) approach.
-        """
+    def execute_command(self, command_name: str):
+        """Execute a previously registered command by name."""
         try:
-            self.commands[command_name].execute(*args)
+            self.commands[command_name].execute()
         except KeyError:
             print(f"No such command: {command_name}")
+
+    def list_commands(self):
+        """Print out all registered commands."""
+        for index, command_name in enumerate(self.commands, start=1):
+            print(f"{index}. {command_name}")
+
+    def get_command_by_index(self, index: int):
+        """Return the command name by 0-based index, or None if invalid."""
+        try:
+            command_name = list(self.commands.keys())[index]
+            return command_name
+        except IndexError:
+            return None
