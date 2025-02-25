d5646b14e5a6834ab593cca579e0c900c3e0f0c7
d3v07 <trivedidev16.com>
2025-02-25 18:36:13 -0500

    refactor: remove unused exit command module

--- app/commands/exit/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-from app.commands import Command
-class Exitcommand:
-    def execute(self, *args):
-        """Exits the application."""
-        print("Exiting... Goodbye!")
-        sys.exit()
